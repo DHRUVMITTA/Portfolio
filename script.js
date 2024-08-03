4dc57e9aaf76ea7af84c95054485a32472a341c8
Dhruv Mittal <dhruvmittal4480@gmail.com>
2024-08-03 20:02:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + "px";
+  crsr.style.top = dets.y + "px";
+  blur.style.left = dets.x - 250 + "px";
+  blur.style.top = dets.y - 250 + "px";
+});
+gsap.to(".nav",{
+    backgroundColor:"black", height:"13vh",
+    scrollTrigger:{
+        trigger:".nav",
+        scroller:"body",
+        start:"top - 10%",
+       
+        scrub:2
+    }
+});
